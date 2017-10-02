6f5b9e20739451ec9237d306cabe3133d1a82391
Shohei1029 <t14650sn@sfc.keio.ac.jp>
2017-10-02 15:25:55 +0900

    :star2:

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -53,6 +53,20 @@ if [ ! `which zplug` ];then
     curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 fi
 
+#pyenv
+if [ ! `which pyenv` ];then
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+    echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshenv
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshenv
+    echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshenv
+fi
+#pyenv-update (pyenv plugin)
+git clone git://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
+
+
 
 #tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
