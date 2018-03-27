9666d7446efdfac59942f5d349480992edd2bccc
Shohei1029 <t14650sn@sfc.keio.ac.jp>
2018-03-27 11:39:38 +0900

    modify init.sh

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -53,35 +53,8 @@ if [ `uname` = "Darwin" ]; then
     brew cleanup
 
 elif [ `uname` = "Linux" ]; then
-    unset LD_LIBRARY_PATH
-    echo "unset LD_LIBRARY_PATH"
-    echo "installing Linuxbrew..."
-    which brew >/dev/null 2>&1 || sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-    #試してないので不安しかない
-    test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-    test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
-    test -r ~/.bash_profile && echo 'export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"' >>~/.bash_profile
-    echo 'export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"' >>~/.profile
-    echo 'export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"' >>~/.zshrc.local
-
-    formulae=(
-        gcc
-        git
-        zsh
-        tmux
-        ag
-#        "vim --with-python3 --with-luajit" #kingでうまく入らなかった
-        aria2
-    )
-
-#    echo "brew tap..."
-
-    echo "brew install apps..."
-    for formula in "${formulae[@]}"; do
-        brew install $formula || brew upgrade $formula
-    done
-
-    brew cleanup
+    #if [ ! `which zsh` ];then
+    # zshのインストール処理を入れたい
 
     # neovim #-> brewよりさらに楽で確実に入れられる
     curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
