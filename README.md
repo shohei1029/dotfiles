a8832dd220111f0448e174eaa74ea2b133faf2b7
shohei1029 <shohei@sfc.keio.ac.jp>
2019-10-19 14:16:53 +0900

    Create develop branch. \n Added install.sh

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 # dotfiles
+for macOS or Ubuntu
+
+## Setup
+```sh
+$ curl -sSL https://raw.githubusercontent.com/shohei1029/dotfiles/master/install.sh | sh
+```
+
+## Misc.
 
 ### iTerm2
 `git clone https://github.com/rickhanlonii/Solarized-Darcula/`
 
 ### zsh
-
- 
-// Shellのデフォルトに設定
+- Shellのデフォルトに設定  
 `$ chsh -s /bin/zsh`
-
-//Updating
+- Updating  
 `git pull && git submodule update --init --recursive`
 
-#####powerline対応のRicty利用準備
+##### powerline対応のRicty利用準備
 ```sh
 $ brew tap sanemat/font
 $ brew reinstall --powerline --vim-powerline ricty
 $ cp -f /usr/local/Cellar/ricty/3.2.4/share/fonts/Ricty*.ttf ~/Library/Fonts/
 $ fc-cache -vf
- Rictyを指定
+ #Rictyを指定
 ```
+
+
+## References
+- [PiroPiro](https://github.com/PiroHiroPiro/dotfiles)
---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/zsh
+
+DOTPATH=~/.dotfiles
+
+# git が使えるなら git
+if test -n $(which git); then
+  git clone --recursive "https://github.com/shohei1029/dotfiles.git" $DOTPATH
+
+# 使えない場合は curl か wget を使用する
+elif test -n $(which curl) || test -n $(which wget); then
+  if test -n $(which curl); then
+    curl -sSL "https://github.com/shohei1029dotfiles/archive/master.tar.gz" -o $DOTPATH
+  elif test -n $(which wget); then
+    wget -O - "https://github.com/shohei1029/dotfiles/archive/master.tar.gz"
+  fi | tar zxv
+
+  # 解凍したら，DOTPATH に置く
+  mv -f dotfiles-master $DOTPATH
+
+  # 解凍したら，DOTPATH に置く
+  rm -rf dotfiles-master
+else
+  echo "curl or wget required"
+  exit 1
+fi
+
+cd "${DOTPATH}"
+
+if [ $? -ne 0 ]; then
+  echo "not found: ${DOTPATH}"
+  exit 1
+fi
+
+# makeの確認
+if test -z $(which make); then
+  if [ "$(uname)" == "Darwin" ]; then
+    xcode-select --install
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt install -y build-essential
+  fi
+fi
+
+# 移動できたらmakeで実行する
+make init
+
+# 終了
+exit 0
